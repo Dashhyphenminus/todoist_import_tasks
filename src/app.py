35bb609d77e7684002ff5c56f10311ff4a2d60c3
Dashhyphenminus <robertblendea@outlook.com>
2023-09-07 21:36:21 -0700

    jlksdfajlksdj yayyy

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 # import importlib.util
 # import sys
+from todoist_api_python.api import TodoistAPI
 import re
 
 
@@ -40,7 +41,7 @@ def icsToString(file):
 
 def stringToEvents(string):
     events = []
-    events.append(Event("title", "date", 100, "description", "url"))
+    events.append(Event("title", 100, 100, "description", "url"))
 
     i = 0
     while (i < len(string)):
@@ -48,11 +49,23 @@ def stringToEvents(string):
         i = string.find("BEGIN:VEVENT", i)
         if (i == -1):
             break
-        DTstamp = string[string.find("DTSTAMP:", i) + 8 : string.find("\n", string.find("DTSTAMP:", i) + 8)]
-        print (DTstamp)
+        dtStamp = string[string.find("DTSTAMP:", i) + 8 : string.find("\n", string.find("DTSTAMP:", i) + 8)]
+        uId = string[string.find("UID:", i) + 8 : string.find("\n", string.find("UID:", i) + 4)]
+        dTStart = string[string.find("DTSTART:", i) + 8 : string.find("\n", string.find("DTSTART:", i) + 8)]
+        dTEnd = string[string.find("DTEND:", i) + 8 : string.find("\n", string.find("DTEND:", i) + 6)]
+        description = string[string.find("DESCRIPTION:", i) + 8 : string.find("\n", string.find("DTSTAMP:", i) + 12)]
+        location = string[string.find("LOCATION:", i) + 8 : string.find("\n", string.find("LOCATION:", i) + 9)]
+        summary = string[string.find("SUMMARY:", i) + 8 : string.find("\n", string.find("SUMMARY:", i) + 8)]
+        url = string[string.find("URL:", i) + 8 : string.find("\n", string.find("URL:", i) + 4)]
+
+
+
+        thing = Event(summary, dtStamp, dTStart, description, url)
+        events.append (thing)
         
         i += 1
 
+    print (events)
         
         
 
